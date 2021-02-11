142c6d54c469ff86a37b2f15604531183cee4866
Alex Reyes <reyesruizalex@gmail.com>
2021-02-11 18:42:30 -0500

    Update book list

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # http://www.alexreyes.xyz
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/a9edc2f9-3a4a-4bf8-bc53-22ee8a4480d5/deploy-status)](https://app.netlify.com/sites/alexreyespersonal/deploys)
--- assets/js/bookList.js
+++ assets/js/bookList.js
@@ -1,24 +1,78 @@
 
 $(document).ready(function () {
-    // Replace ./data.json with your JSON feed
-    fetch('https://cors-anywhere.herokuapp.com/https://reyes-personal-website-api.herokuapp.com/book2019')
+
+    // 2021: 
+    fetch('https://flaask.alex243.repl.co/?year=2021')
     .then(response => {
     return response.json()
     })
     .then(data => {
-        var data = data[0]
+        var total = data.length;
+        $('.2021').hide().append('<h2 class="year"> 2021 (' + total + ' total)</h2>').fadeIn(0);
+        
+        data.forEach(book => {
+            if (book) {
+                var link = book[1]
+
+                var bookTitle = book[0]
+
+                if (book != ""){ 
+                
+                    $('.displayList2021').hide().append('<p class = "indent"><a href = ' + link + 'class = "no-underline">' + bookTitle + '</a></p>').fadeIn(0);
+                }
+            }
+        });
+    })
+    .catch(err => {
+        // Do something for an error here
+        console.log("error reading JSON")
+    })
+
+    // 2020: 
+    fetch('https://flaask.alex243.repl.co/?year=2020')
+    .then(response => {
+    return response.json()
+    })
+    .then(data => {
+        var total = data.length;
+        $('.2020').hide().append('<h2 class="year"> 2020 (' + total + ' total)</h2>').fadeIn(0);
+        
+        data.forEach(book => {
+            if (book) {
+                var link = book[1]
+
+                var bookTitle = book[0]
+
+                if (book != ""){ 
+                
+                    $('.displayList2020').hide().append('<p class = "indent"><a href = ' + link + 'class = "no-underline">' + bookTitle + '</a></p>').fadeIn(0);
+                }
+            }
+        });
+    })
+    .catch(err => {
+        // Do something for an error here
+        console.log("error reading JSON")
+    })
+
+    // 2019: 
+    fetch('https://flaask.alex243.repl.co/?year=2019')
+    .then(response => {
+    return response.json()
+    })
+    .then(data => {
         var total = data.length;
         $('.2019').hide().append('<h2 class="year"> 2019 (' + total + ' total)</h2>').fadeIn(0);
         
         data.forEach(book => {
             if (book) {
-                var link = book.link
+                var link = book[1]
 
-                var bookTitle = book.title
+                var bookTitle = book[0]
 
                 if (book != ""){ 
                 
-                    $('.displayList').hide().append('<p class = "indent"><a href = ' + link + 'class = "no-underline">' + bookTitle + '</a></p>').fadeIn(0);
+                    $('.displayList2019').hide().append('<p class = "indent"><a href = ' + link + ' target="_blank" class="no-underline">' + bookTitle + '</a></p>').fadeIn(0);
                 }
             }
         });
